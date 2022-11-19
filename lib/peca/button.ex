b7fb98cc36fc2ef7e33852d95bacc54ef4ab1eb7
Bryan Weber <bryan@cobenian.com>
2022-11-19 15:27:48 -0500

    remove io puts

--- lib/peca/button.ex
+++ lib/peca/button.ex
@@ -67,8 +67,6 @@ defmodule Peca.Button do
   slot(:inner_block, doc: "Content of the button.")
 
   def button(assigns) do
-    IO.puts("button assigns: #{inspect(assigns, pretty: true)}")
-
     assigns =
       assigns
       |> handle_class_assigns()
